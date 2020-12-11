cb0981e12f1940d4118fb3a4885ee741ac53c5d9
Алексей Кармильчик <alexey.karm@mail.ru>
2020-12-11 14:00:28 +0300

    10.1 Fix errors, final

--- NewYearGift/DAL/Repositories/JsonDataRepository.cs
+++ NewYearGift/DAL/Repositories/JsonDataRepository.cs
@@ -6,21 +6,26 @@ namespace NewYearGift.DAL.Repositories.Interfaces
 {
     public class JsonDataRepository : IDataRepository
     {
-        private const string Path = @".\App\Data\data.json";
+ 
+        private string GetDataPath()
+        {
+            var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            path = path.Remove(path.LastIndexOf("\\")) + "\\App\\Data\\data.json";
+
+            return path;
+        }
 
         public void SaveData(JsonDataModel data)
         {     
-
             string serializedObject = JsonConvert.SerializeObject(data);
 
-            var res = Path;
-
-            File.WriteAllText(Path, serializedObject);
+            File.WriteAllText(GetDataPath(), serializedObject);
         }
 
         public JsonDataModel ReadData()
         {
-            string jsonText = File.ReadAllText(Path);
+            string jsonText = File.ReadAllText(GetDataPath());
 
             var settings = new JsonSerializerSettings
             {
