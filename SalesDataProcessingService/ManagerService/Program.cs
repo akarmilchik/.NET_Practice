e273bef349f41c5022f39da85fd25826145d211d
Алексей Кармильчик <alexey.karm@mail.ru>
2021-01-20 23:05:05 +0300

    1. Initial. Create project.

---
+++ SalesDataProcessingService/ManagerService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManagerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, effective manager!");
+        }
+    }
+}
